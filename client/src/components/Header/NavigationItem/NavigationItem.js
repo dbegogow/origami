5324fa074d4ce9d18ee50b56e02240bd00e8f9b6
Dzhulio Begogov <dzhulio_begogov@abv.bg>
2021-05-07 21:40:17 +0300

    Change configuration files

--- client/src/components/Header/NavigationItem/NavigationItem.js
+++ client/src/components/Header/NavigationItem/NavigationItem.js
@@ -2,8 +2,8 @@ import style from './NavigationItem.module.css';
 
 const NavigationItem = (props) => {
     return (
-        <li class={style.listItem}>
-            <a href="#" className={style.navListItem}>{props.children}</a>
+        <li className={style.listItem}>
+            <a href="" className={style.navListItem}>{props.children}</a>
         </li>
     );
 };
