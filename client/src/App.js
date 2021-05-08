b018d1653a75436a3c13ee5f2a2f72bd770a0ba0
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-05-09 01:28:10 +0300

    Change App

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
+import { Route, Switch } from 'react-router-dom';
+
 import style from './App.module.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header';
 import Menu from './components/Menu';
+import About from './components/About';
+import ContactUs from './components/ContactUs';
 
 function App() {
     return (
         <div className={style.app}>
             <Header />
-            <Menu />
+            <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/contact-us">
+                    <ContactUs />
+                </Route>
+                <Route path="/">
+                    <Menu />
+                </Route>
+            </Switch>
             <Footer />
         </div>
     );
