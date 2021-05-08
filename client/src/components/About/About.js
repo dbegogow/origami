a30427ce252c81235ea565adfd6671b07a23e9b7
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-05-09 01:29:13 +0300

    Create About Component and ContactUs Component

---
+++ client/src/components/About/About.js
@@ -0,0 +1,5 @@
+import style from './About.module.css';
+
+const About = () => <h1 className={style.container}>About Page</h1>;
+
+export default About;
---
+++ client/src/components/ContactUs/ContactUs.js
@@ -0,0 +1,5 @@
+import style from './ContactUs.module.css';
+
+const ContactUs = () => <h1 className={style.container}>Contact Us Page</h1>;
+
+export default ContactUs;
