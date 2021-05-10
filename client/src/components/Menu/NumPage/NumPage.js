cc02c4cafb2ee94ae35be112109cced7fa9a4e01
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-05-10 19:21:28 +0300

    Made changes in NumPage Component

--- client/src/components/Menu/NumPage/NumPage.js
+++ client/src/components/Menu/NumPage/NumPage.js
@@ -1,11 +1,9 @@
 import menuStyle from '../Menu.module.css';
 
 const NumPage = ({ match }) => {
-    const pageNum = match.url.substring(7);
-
     return (
         <main className={menuStyle.main}>
-            <h1 className={menuStyle.heading}>You are at page numbeerrrr {pageNum}</h1>
+            <h1 className={menuStyle.heading}>You are at page numbeerrrr {match.params.pageNum}</h1>
         </main>
     );
 };
